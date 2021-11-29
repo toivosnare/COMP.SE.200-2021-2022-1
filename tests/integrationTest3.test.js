8b69ec4564073a21fec12781ebc42f6f6ae1e0b3
Toivo Snåre <topisn@gmail.com>
2021-11-29 20:10:06 +0200

    Add integration test with words, map, endsWith and reduce

---
+++ tests/integrationTest3.test.js
@@ -0,0 +1,20 @@
+import words from '../src/words.js'
+import map from '../src/map.js'
+import endsWith from '../src/endsWith.js'
+import reduce from '../src/reduce.js'
+
+test("Product prices are parsed from string to float with euro currency", () => {
+    const input = "146.5€ 472.1£ 841.6€ 741.1$ 791.1£ 239.1$ 13.6£ 865.4$ 891.3€ 172.8$";
+    const values = map(words(input, /[^\s]+/g), (str) => {
+        const amount = parseFloat(str.slice(0, -1));
+        let value;
+        if (endsWith(str, '€'))
+            value = amount;
+        if (endsWith(str, '$'))
+            value = amount * 0.85;
+        if (endsWith(str, '£'))
+            value = amount * 1.15;
+        return Math.round(value * 100) / 100;
+    });
+    expect(reduce(values, (t, v) => t + v, 0.0)).toEqual(5063.38);
+});
